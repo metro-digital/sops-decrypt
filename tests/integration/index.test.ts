7c0b2dddb03c7e0c92434db02643f9bc0a001c62
Navya Teja Otturu <navya.teja@metronom-external.com>
2020-11-30 11:38:27 +0530

    Rename the function to reflect what it is doing
    Also, variable and function using the same name impacts the readablity.
    Hence, changing the name of the function to reflect exactly what it is
    doing.

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -8,7 +8,7 @@ describe('When deleting the gpg key', () => {
 
   it('should be able to get the fingerprint of the key passed', async ()=>{
     let expectedFPR = "B4226A1192494789A9402116A344AC03500C6401"
-    let actualFPR = await gpg.fingerprint(gpg_keys.base64_private_key1)
+    let actualFPR = await gpg.get_fingerprint(gpg_keys.base64_private_key1)
 
     expect(actualFPR).toEqual(expectedFPR)
   })
