f476ed43b8e54ddb40286e0bcd217bf421b26bfb
Blazej Budzynski <blazej.budzynski@metronom-external.com>
2021-04-09 09:01:49 +0200

    Fix masking multiline strings

--- src/action.ts
+++ src/action.ts
@@ -59,6 +59,14 @@ function hideSecrets (result: string, outputFormat: string) :void {
   }
 
   for (const property in obj) {
-    core.setSecret(obj[property])
+    const val = '' + obj[property]
+    if (val.indexOf('\n') === -1) {
+      core.setSecret(val)
+    } else {
+      // setSecret does not support multiline strings
+      for (const line of val.split('\n')) {
+        core.setSecret(line)
+      }
+    }
   }
 }
